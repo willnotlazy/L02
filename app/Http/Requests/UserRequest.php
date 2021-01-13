79535a7036daaf7152d1a83eb43eb4b3cf1a5556
willnotlazy <519478450@qq.com>
2021-01-13 01:46:00 +0000

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -27,13 +27,16 @@ class UserRequest extends FormRequest
         return [
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_ ]+$/|unique:users,name,'.Auth::id(),
             'email' => 'required|email',
-            'introduction' => 'max:80'
+            'introduction' => 'max:80',
+            'avatar' => 'mimes:jpg,jpeg,png,gif,bmp|dimensions:min_width=208,min_height=208'
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mimes' => '不支持的头像格式',
+            'avatar.dimensions' => '图片清晰度至少为208x208',
             'name.unique' => '用户名已被占用, 请重新填写',
             'name.regex'  => '用户名只支持英文、数字、横杠和下划线。',
             'name.between' => '用户名必须介于3-25个字符之间。',
